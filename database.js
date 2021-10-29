1b16b10e2e33bc317bd817f2e581b67f56c1da1e
laelion <lae@jphlions.com>
2021-10-29 17:59:46 -0300

    reloj1.4

--- database.js
+++ database.js
@@ -1,8 +1,7 @@
-const reqdate = require ("./fdate.js")
+const reqdate = require("./fdate.js")
 const express = require("express");
 const app = express();
 const sql = require('mssql')
-
 const sqlConfig = {
   user: "lion",
   password: "123",
@@ -14,82 +13,106 @@ const sqlConfig = {
     idleTimeoutMillis: 30000
   },
   options: {
-    encrypt: true, 
-    trustServerCertificate: true 
+    encrypt: true,
+    trustServerCertificate: true
+  }
+}
+const conexion = new sql.ConnectionPool(sqlConfig);
+const request =  new sql.Request(conexion);
+
+async function seleccionar(req, res) {
+  try {
+    let respuesta = hacerElSelectALaBaseDeDatos()
+    console.log(respuesta)
+    res.json(respuesta)
+  }
+  catch (errores) {
+    console.log(errores)
   }
 }
 
-const conexion = new sql.ConnectionPool(sqlConfig);
-const request = new sql.Request(conexion);
-const queryS = "SELECT * FROM reloj"
-const queryR = "DELETE FROM RELOJ"
+async function seleccionarSinRES() {
+  try {
+    let respuesta = hacerElSelectALaBaseDeDatos()
+    respuesta
+  }
+  catch (errores) {
+    console.log(errores)
+  }
+}
+
+async function insertar(req, res) {
+  let date = reqdate.parsearFecha(req.query.fecha)
+  let reloj = reqdate.armarJson(date)
+  try {
+    const resulatosS = await seleccionarSinRES()
+    console.log(resulatosS)
+    if (resulatosS.rowsAffected == 0) {
+        hacerElinsertALaBaseDeDatos(reloj)
+      res.send(reloj)
+    } else {
+        hacerElmodificarALaBaseDeDatos(reloj)
+      res.send(reloj)
+    }
+  } catch (errores) {
+    console.log(errores)
+  }
+}
+
+//Borrar
+async function borrar(req, res) {
+  try {
+    let resultadoD =  hacerElEliminarALaBaseDeDatos()
+    console.log(resultadoD)
+    if (resultadoD.rowsAffected != 0) {
+      res.send("Campos borrados correctamente");
+    } else {
+      res.send("No hay campos para borrar");
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 async function conectar() {
-  await conexion.connect()
-}
-
-
-
-//Seleccionar
-async function seleccionar(req, res){
-  try{    
-      await conectar();
-      let resultado = await  request.query(queryS)
-      res.json(resultado)
-  }
-  catch(errores){
-      console.log(errores)
-  }
-  }
-//Seleccionas sin Json
-  var seleccionarSinJson = async function (req, res){
-    try{    
-        await conectar()
-        let resultadoS = await request.query(queryS)
-        return resultadoS
-        
-    }
-    catch(errores){
-        console.log(errores)
-    }
-    }
-
-//Insertar o Modificar
-async function insertar(req, res){
  
-        let date = reqdate.parsearFecha(req.query.fecha)
-        let reloj = reqdate.armarJson(date)
-          try {
-              const resulatosS = await seleccionarSinJson()
-              if(resulatosS.rowsAffected==0){
-               let insertar =  await request.query("INSERT INTO reloj values (" 
-               + reloj.Año + "," + reloj.Mes + "," + reloj.Dia + "," + reloj.Hora + "," + reloj.Minutos + "," + reloj.Segundos + ")")
-               console.log(insertar)
-               res.send(reloj)
-                }else{
-                let modificar = await request.query("UPDATE reloj set año=" + reloj.Año + ", mes=" + reloj.Mes + ", dia= " + reloj.Dia
-                + ", hora=" + reloj.Hora + ", minutos= " + reloj.Minutos + ", segundos= " + reloj.Segundos)
-                console.log("modificar")
-                res.send( reloj)
-              }
-                }catch(errores){
-                console.log(errores)
-                }
+ 
+     conexion.connect()
 }
 
-      //Borrar
-async function borrar(req, res){
-        try {
-          await conectar()
-          let resultadoD = await request.query(queryR)
-          if (resultadoD.rowsAffected!=0){
-            res.send("Campos borrados correctamente");
-        }else{
-            res.send("No hay campos para borrar");
-        }
-        } catch (error) {
-          console.log(error)
-        }
-}              
+var hacerElSelectALaBaseDeDatos = async function () {
+   ejecutarquery("SELECT * FROM reloj")
+}
 
-module.exports = {sql, sqlConfig, conexion, request, seleccionar, insertar, borrar}
+var hacerElmodificarALaBaseDeDatos = async function (reloj) {
+   ejecutarquery("UPDATE reloj set año=" + reloj.Año + ", mes=" + reloj.Mes + ", dia= " + reloj.Dia
+  + ", hora=" + reloj.Hora + ", minutos= " + reloj.Minutos + ", segundos= " + reloj.Segundos)
+}
+
+var hacerElinsertALaBaseDeDatos = async function (reloj) {
+   ejecutarquery("INSERT INTO reloj values (" + reloj.Año + "," + reloj.Mes + "," + reloj.Dia + "," + reloj.Hora + "," + reloj.Minutos + "," + reloj.Segundos + ")")
+
+}
+
+var hacerElEliminarALaBaseDeDatos = async function () {
+    ejecutarquery("DELETE FROM reloj")
+
+}
+
+var ejecutarquery = async function (query) {
+  try{
+    await conectar()
+    let respuesta = await request.query(query)
+    return respuesta
+  }
+ catch(error){
+  console.log(error)
+ }
+}
+
+
+module.exports = { 
+  seleccionar, 
+  insertar, 
+  borrar 
+}
